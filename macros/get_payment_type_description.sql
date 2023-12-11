b9b5c0a4e2ba0079bcc54ff4870ff7937e07d0b2
Jeremy Viray <jetemy@gmail.com>
2023-12-11 22:09:19 +0000

    added testing and documentation requirements in schema.yml

---
+++ macros/get_payment_type_description.sql
@@ -0,0 +1,16 @@
+{#
+    This macro takes payment type and returns description
+#}
+
+{% macro get_payment_type_description(payment_type) -%}
+
+	case {{ payment_type }}
+		when 1 then 'Credit Card'
+		when 2 then 'Cash'
+		when 3 then 'No Charge'
+		when 4 then 'Dispute'
+		when 5 then 'Unknown'
+		when 6 then 'Voided Trip'
+	end
+
+{%- endmacro %}
